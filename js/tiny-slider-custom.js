5c1232348f044bcdcab31d72901b8d1332c59fbe
PC Magik - Home Lab <101038643+pcmagik@users.noreply.github.com>
2023-05-16 02:46:24 +0200

    start of really true work

---
+++ js/tiny-slider-custom.js
@@ -0,0 +1,11 @@
+   var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoHeight: true,
+    controls: false,
+    nav: false,
+    autoplayButtonOutput: false,
+    autoplayTimeout: 5000,
+    autoplay: true
+   });
